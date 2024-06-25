f5df684435e8372a6e61e0ee431c53a91998c14f
a.shamsalahi <a.shamsalahi@drdr.doctor>
2024-06-25 21:59:32 +0330

    implement binary searchIndex

--- Algorithm/Search/BinarySearch.ts
+++ Algorithm/Search/BinarySearch.ts
@@ -9,3 +9,24 @@ function search(array: number[], value: number): number | undefined {
     return search(array.slice(middleIndex), value);
   }
 }
+
+function searchIndex(array: number[], value: number): number | undefined {
+  function searchByLimits(start: number, end: number): number | undefined {
+    const middleIndex = Math.ceil((end - start) / 2) + start;
+    if (array[middleIndex] === value) {
+      let i = middleIndex;
+      while (array[i - 1] === value) {
+        i--;
+      }
+      return i;
+    }
+    if (start >= end) return -1;
+    if (array[middleIndex] > value) {
+      return searchByLimits(0, middleIndex - 1);
+    } else {
+      return searchByLimits(middleIndex + 1, end);
+    }
+  }
+
+  return searchByLimits(0, array.length - 1);
+}
